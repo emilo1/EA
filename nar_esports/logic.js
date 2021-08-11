8777d77ec9fe2431d255404ea4e17eaccec68b59
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-08-11 19:08:06 -0400

    add nar_esports

--- nar_esports/logic.js
+++ nar_esports/logic.js
@@ -1,1 +1,5 @@
-var queryUrl = ''
+var queryUrl = 'https://api.sportradar.us/csgo-t1/en/schedules/2016-06-05/schedule.json?api_key=h55zmfc8rnc4znu9z3wp7m6h'
+   
+d3.json(queryUrl).then(function(data) {
+    console.log(data)
+})
