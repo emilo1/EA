8975055d8179ffb2676d92866bd30e9025f79eb9
Norman Robinson <normanrobinson@normans-mbp.attlocal.net>
2021-08-12 12:10:07 -0400

    nar_esports

--- nar_esports/logic.js
+++ nar_esports/logic.js
@@ -1,5 +1,7 @@
-var queryUrl = 'https://api.sportradar.us/csgo-t1/en/schedules/2016-06-05/schedule.json?api_key=h55zmfc8rnc4znu9z3wp7m6h'
-   
-d3.json(queryUrl).then(function(data) {
-    console.log(data)
-})
+var queryUrl = "https://api.pandascore.co/teams/1654?token=hgZFSXMTMMpiFPPcJKzi2Myq0ThrBJJDJnfqU84l36Y1hHf6UIg";
+
+console.log(queryUrl)
+
+
+
+
